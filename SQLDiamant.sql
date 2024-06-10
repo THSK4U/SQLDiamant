43d67f842336b0354703547f76e4b9f581008a92
TAHA SELLAK <sellaktaha1@gmail.com>
2024-06-10 19:03:19 +0100

    Tache 4

--- SQLDiamant.sql
+++ SQLDiamant.sql
@@ -21,6 +21,7 @@ CREATE TABLE Mouvements (
     FOREIGN KEY (id_room) REFERENCES Rooms(id_room)
 );
 
+
 CREATE TABLE Objets (
   id_objet Int primary key,
   nom_objet varchar(255),
@@ -45,16 +46,25 @@ INSERT INTO rooms (id_room, nom) VALUES (2, 'Bibliothèque');
 INSERT INTO rooms (id_room, nom) VALUES (3, 'Cuisine'); 
 INSERT INTO rooms (id_room, nom) VALUES (4, 'Salon'); 
 INSERT INTO rooms (id_room, nom) VALUES (5, 'Jardin');
-INSERT INTO Mouvements (id, id_invite, id_room, time) VALUES (1, 1, 1, 20); 
-INSERT INTO Mouvements (id, id_invite, id_room, time) VALUES (2, 2, 2, 20);
- INSERT INTO Mouvements (id, id_invite, id_room, time) VALUES (3, 3, 1, 21); 
- INSERT INTO Mouvements (id, id_invite, id_room, time) VALUES (4, 4, 3, 21); 
- INSERT INTO Mouvements (id, id_invite, id_room, time) VALUES (5, 5, 2, 22);
+INSERT INTO Mouvements (id,  id_invite, id_room, time) VALUES
+(1,1, 1, '2023-06-01'),
+(2,2, 1, '2023-06-01'),
+(3,3, 2, '2023-06-01'),
+(4,4, 3, '2023-06-01'),
+(5,1, 2, '2023-06-02');
 INSERT INTO Objets (id_objet, nom_objet, id_room) VALUES (1, 'Vase Ancien', 1); 
 INSERT INTO Objets (id_objet, nom_objet, id_room) VALUES (2, 'Tableau de Maître', 2); 
 INSERT INTO Objets (id_objet, nom_objet, id_room) VALUES (3, 'Statue en Bronze', 3);
  INSERT INTO Objets (id_objet, nom_objet, id_room) VALUES (4, 'Coffre à Bijoux', 2);
  INSERT INTO Objets (id_objet, nom_objet, id_room) VALUES (5, 'Lampe en Cristal', 4);
- 
+ INSERT INTO Indices (id_indice, description, id_room) VALUES (1, 'Empreintes de pas trouvées près du vase', 1); 
+ INSERT INTO Indices (id_indice, description, id_room) VALUES (2, 'Traces de sang sur la table', 2); 
+ INSERT INTO Indices (id_indice, description, id_room) VALUES (3, 'Foulard déchiré retrouvé sur le sol', 3); 
+ INSERT INTO Indices (id_indice, description, id_room) VALUES (4, 'Clé ancienne trouvée sous le canapé', 4); 
+ INSERT INTO Indices (id_indice, description, id_room) VALUES (5, 'Bijou abandonné dans le jardin', 5);
+
 select * from Invités;
 select nom,statut from Invités;
+select count(*) from invités;
+select count(*) from invités where statut = 'Suspect';
+
